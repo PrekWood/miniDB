f0faa8a2901fbaea137c3ce9b50358fade42a247
Ilias Brinias <elliotpatra@gmai.com>
2021-01-13 21:32:16 +0200

    start to make the linked list = bucket list

--- hashIndex.py
+++ hashIndex.py
@@ -2,25 +2,27 @@ import hashlib
 
 
 class Hashlisted:
-    def __init__(self, b):
-        '''
-        The tree abstraction.
-        '''
-        self.b = b # branching factor
-        self.nodes = [] # list of nodes. Every new node is appended here
-        self.root = None # the index of the root node
+    def __init__(self):
+        self.headval = None
+
+
+class Bucket:
+    def __init__(self, value = None):
+        self.value = None
+        self.next = None
+        self.hashfact = None
 
 
 
-class Node:
-    def __init__(self, b, id=[], ptrs=[]):
-        self.b = b # branching factor
-        self.id = id # Values (the data from the pk column)
-        self.ptrs = ptrs # ptrs (the indexes of each datapoint or the index of another bucket)
+list1 = SLinkedList()
+list1.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+# Link first Node to second node
+list1.headval.nextval = e2
 
-
-
-
+# Link second Node to third node
+e2.nextval = e3
 
 
 
@@ -28,17 +30,20 @@ def hasfuct(i):
     return i % 2
 
 
-hash_table = [[] for _ in range(2)]
+    hash_table = [[] for _ in range(2)]
 
-data = {'25': 'USA',
-        '20': 'India',
-        '10': 'Nepal',
-        '22': 'Greece'}
+    data = {'25': 'USA',
+    '20': 'India',
+    '10': 'Nepal',
+    '22': 'Greece'}
 
-for i in data:
-    bucket = hasfuct(int(i))
-    if len(hash_table[bucket]) == 2:
-        print("bucket :", bucket, " is full")
-    else:
-        hash_table[bucket].append(i)
-print(hash_table)
+    for i in data:
+        bucket = hasfuct(int(i))
+        if len(hash_table[bucket]) == 2:
+            print("bucket :", bucket, " is full")
+        else:
+            hash_table[bucket].append(i)
+            print(hash_table)
+
+
+# https://www.tutorialspoint.com/python_data_structure/python_linked_lists.htm
