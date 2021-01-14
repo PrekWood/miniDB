bd9e899e7946a92f54c2c08c66ee7e9a65dd3b13
Ilias Brinias <elliotpatra@gmai.com>
2021-01-14 13:51:17 +0200

    i create the list bucket and tried to create the find faction

--- hashIndex.py
+++ hashIndex.py
@@ -12,7 +12,7 @@ class Hashlisted:
 
 class NodeBucket:
     # every bucket has a list for the values and a list for the next buckets
-    def __init__(self, id, size=5, hashfact=2, value=None):
+    def __init__(self, id, size=2, hashfact=3, value=None):
         self.id = id                # id to recognize each bucket
         self.value = value          # a list with indexes to the DB file
         self.size = size            # how many id for the DB can handle
@@ -21,9 +21,6 @@ class NodeBucket:
 
     def find(self, value, hashfuct):
         bucket = value % hashfuct
-        while True:
-            if self.next[bucket].next is None:
-                break
         if value in self.next[bucket].value:
             print("find it in bucket ", self.next[bucket].id)
 
@@ -34,11 +31,14 @@ list1 = Hashlisted()
 list1.headval = NodeBucket(-1)
 
 # create first bucket layer
-bucket2 = NodeBucket('1st', value=[9, 5])
-bucket1 = NodeBucket('1st', value=[2, 4])
+bucket1 = NodeBucket('1st', value=[3, 6])
+bucket2 = NodeBucket('2st', value=[1, 4])
+bucket3 = NodeBucket('3st', value=[2, 5])
 # connect these buckets to the root bucket with id = -1
 list1.headval.next.append(bucket1)
 list1.headval.next.append(bucket2)
+list1.headval.next.append(bucket3)
+i = list1.headval.next
 # try to create the find faction
 list1.headval.find(5, list1.headval.hashfact)
 # https://www.tutorialspoint.com/python_data_structure/python_linked_lists.htm
