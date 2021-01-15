bb84d9bd26ff8f283cde2bdd0b359f9d0e1a0218
Ilias Brinias <elliotpatra@gmai.com>
2021-01-15 13:52:39 +0200

    find faction completed

--- hashIndex.py
+++ hashIndex.py
@@ -1,10 +1,3 @@
-import hashlib
-
-
-def hasfuct(i):
-    return i % 2
-
-
 class Hashlisted:
     def __init__(self):
         self.headval = None  # root bucket
@@ -12,33 +5,91 @@ class Hashlisted:
 
 class NodeBucket:
     # every bucket has a list for the values and a list for the next buckets
-    def __init__(self, id, size=2, hashfact=3, value=None):
-        self.id = id                # id to recognize each bucket
-        self.value = value          # a list with indexes to the DB file
-        self.size = size            # how many id for the DB can handle
-        self.next = []              # for next linked bucket, this is for the overflow situation
-        self.hashfact = hashfact    # hash fuction for the next linked buckets
+    def __init__(self, id, size=2, hashfact=3, value=[], maxhashfaction=10, isleaf=True):
+        self.id = id  # id to recognize each bucket
+        self.value = value  # a list with indexes to the DB file
+        self.size = size  # how many id for the DB can handle
+        self.next = []  # for next linked bucket, this is for the overflow situation
+        self.hashfact = hashfact  # hash faction for the next linked buckets
+        self.maxhashfaction = maxhashfaction  # maximum buckets that can create a single bucket after overflow
+        self.isleaf = isleaf
 
-    def find(self, value, hashfuct):
-        bucket = value % hashfuct
-        if value in self.next[bucket].value:
-            print("find it in bucket ", self.next[bucket].id)
+    def __hashfuction__(self, value):
+        return value % self.hashfact
+
+    def find(self, value):
+        bucket = self.__hashfuction__(value)
+        current_bucket = self.next[bucket]
+        # find the bucket tha is a leaf or the attribute next == []
+        # this loop will be stop when we find the bucket - leaf
+        while not current_bucket.isleaf:
+            current_bucket = current_bucket.next[current_bucket.__hashfuction__(value)]
+        # if the value is inside the list bucket.value returns the bucket id
+        if value in current_bucket.value:
+            print("find it in bucket ", current_bucket.id)
+
+    def insert(self, value):
+        bucket = self.__hashfuction__(value)
+        # if this bucket is not full
+        current_bucket = self.next[bucket]
+        while not current_bucket.isleaf:
+            current_bucket = current_bucket.next[current_bucket.__hashfuction__(value)]
+        if len(current_bucket.value) < current_bucket.size:
+            current_bucket.value.append(value)
+        else:
+            current_bucket.hashfaction = 2
+            current_bucket.isleaf = False
+            newbuckets1 = NodeBucket("newbuckets1")
+            newbuckets2 = NodeBucket("newbuckets2")
+            current_bucket.next = [newbuckets1, newbuckets2]
+        print("ok")
 
 
-# make the list
-list1 = Hashlisted()
-# rout bucket is the node with id -1
-list1.headval = NodeBucket(-1)
+def testfind():
+    # make the list
+    list1 = Hashlisted()
+    # rout bucket is the node with id root
+    list1.headval = NodeBucket("root")
 
-# create first bucket layer
-bucket1 = NodeBucket('1st', value=[3, 6])
-bucket2 = NodeBucket('2st', value=[1, 4])
-bucket3 = NodeBucket('3st', value=[2, 5])
-# connect these buckets to the root bucket with id = -1
-list1.headval.next.append(bucket1)
-list1.headval.next.append(bucket2)
-list1.headval.next.append(bucket3)
-i = list1.headval.next
-# try to create the find faction
-list1.headval.find(5, list1.headval.hashfact)
+    # ------------
+    # test for find faction
+    # create first bucket layer
+    bucket1 = NodeBucket('1st', value=[3, 6])
+    bucket2 = NodeBucket('2st', value=[1, 4])
+    bucket3 = NodeBucket("3st", hashfact=2, isleaf=False)
+    # make a second layer bucket
+    bucket3i = NodeBucket('3st_i', value=[2, 8])
+    bucket3ii = NodeBucket('3st_ii', value=[5])
+    bucket3.next.append(bucket3i)
+    bucket3.next.append(bucket3ii)
+
+    # connect these buckets to the root bucket with id root
+    list1.headval.next.append(bucket1)
+    list1.headval.next.append(bucket2)
+    list1.headval.next.append(bucket3)
+    list1.headval.find(5)
+    # -------------
+
+
+def testinstert():
+    # make the list
+    list1 = Hashlisted()
+    # rout bucket is the node with id root
+    list1.headval = NodeBucket("root")
+    # test for insert faction
+    for i in range(3):
+        list1.headval.next.append(NodeBucket(i))
+    list1.headval.insert(1)
+    list1.headval.insert(2)
+    list1.headval.insert(3)
+    list1.headval.insert(4)
+    list1.headval.insert(5)
+    list1.headval.insert(6)
+    list1.headval.insert(8)
+    # --------------
+    # try to create the find faction
+    list1.headval.find(5)
+
+# testfind()
+testinstert()
 # https://www.tutorialspoint.com/python_data_structure/python_linked_lists.htm
