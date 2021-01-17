706da19e3479038a9ad1d3196119f9d4c2bdc548
prekas <nikosprek2000@gmail.com>
2021-01-17 21:47:23 +0200

    HashIndex Class

--- hash_index.py
+++ hash_index.py
@@ -1,80 +1,126 @@
-class HashBucket:
-    def __init__(self, id, _max_input_size, data = []):
-        self.id = id
-        self._max_input_size = _max_input_size
-        self.data = data
-    # data = [[id,value],[]]
-    def addItem(self, row):
-        # print(self.id, row)
+from database import Database
+import sys
+import copy
+
+class Bucket:
+    def __init__(self, max_bucket_size=64, data=None):
+        if(data == None):
+            self.data = []
+        else:
+            self.data = data
+        self.max_bucket_size = max_bucket_size
+        self.size = sys.getsizeof(self.data)
+
+    def insert(self, row):
         self.data.append(row)
-
-    # def redirect(self):
-    #     redirect_bucket = HashBucketRedirect(self.id)
-
-
-# class HashBucketRedirect:
-#     '''
-#         breakes a bucket into two and redirects the data that was send to be written here to the children buckets
-#     '''
-#     def __init__(self, id):
-#         self.id = id
-#         self.buckets = [
-#
-#         ]
-#
-#     def addItem(self, row):
-#         self.data.append(row)
+        self.size = sys.getsizeof(self.data)
 
 class HashIndex:
-    def __init__(self, db, max_bucket_size=10):
+
+    def __init__(self, db, max_bucket_size=5):
+        # todo: rename count_buckets σε κάτι σαν hashfunctionN γιατι δεν ειναι όντως το count των buckets αλλα το %2^n της hashing function
+        # todo: address table συζητηση
+
+        if type(db) is not Database:
+            raise Exception('db parameter must be an object of the Database class')
+
         self.db = db
         self.max_bucket_size = max_bucket_size
-        self.buckets = {}
-        self.bucket_count = 0
+        self.bucket_list = []
+        self.row = []
+        self.count_buckets = 0
 
-        #loop through tables and create hash indexes
-        for table_index in self.db.tables:
-            table = self.db.tables[table_index]
-            if table_index == "users":
-                self.buckets[table_index] = [
-                    self.addBucket(),
-                    self.addBucket()
-                ]
-                self.createTableIndex(table_index, table.data)
+    def hashFunction(self, input):
+        return input % (2 ** self.count_buckets)
 
-                # print(self.buckets)
+    def createHashtable(self, table, column=0):
+        #todo: to be called by constuctor on primary key
 
-                for b in self.buckets[table_index]:
-                    print(b)
-                    print(b.data)
+        # loop through tables and create hash indexes
+        # we insert a list with the data for the hashing and the pointer
+        # to the db
+        table_rows = self.db.tables[table].data
+        for row_position in range(len(table_rows)):
+            hash_key = table_rows[row_position][column]
+            self.insert([hash_key, row_position])
+
+    def _balanceBuckets(self, value):
+        # todo: overflow after balance
+
+        # the templist has all the values from the bucket
+
+        self.count_buckets += 1
+
+        templist = []
+        for bucket in self.bucket_list:
+            for row in bucket.data:
+                templist.append(row)
+
+        # we add the value (this was the reason for overflow)
+        templist.append(value)
+
+        # we clear the buckets
+        self.bucket_list.clear()
+
+        # we create new buckets
+        for i in range(2 ** self.count_buckets):
+            self.bucket_list.append(Bucket(self.max_bucket_size))
+
+        # we insert all the elements
+        # we are sure that we dont have overflow situation
+        for value in templist:
+            hash_value = self.hashFunction(value[0])
+            self.bucket_list[hash_value].insert(value)
+
+        return self.bucket_list
 
 
-    def addBucket(self):
-        bucket = HashBucket(self.bucket_count+1, self.max_bucket_size)
-        self.bucket_count += 1
-        return bucket
+    def insert(self, value):
+        # if the bucket is not full insert the element
+        try:
+            if self.bucket_list == []:
+                self.bucket_list.append(Bucket(self.max_bucket_size))
 
-    def createTableIndex(self, table_index, data):
-        for row in data:
-            #Hash function returns the selected bucket
-            hash_key = row[0] #todo: primary key
-            bucket_id = self.hashFunction(hash_key)
+            bucket_to_insert = self.bucket_list[self.hashFunction(value[0])]
+            # we use deepcopy because python's simple copying makes it so that both variables point to the same
+            # collection of memory. So this calculation would have been imposible without changing the object's data property
+            bucket_data = copy.deepcopy(bucket_to_insert.data)
+            bucket_data.append(value)
+            bucket_size_after_insertion = sys.getsizeof(bucket_data)
 
-            #bug: Τα rows γράφονται και στα δύο objects
-            bucket_selected = self.buckets[table_index][bucket_id]
-            bucket_selected.data.append(row)
-            # bucket_selected.addItem(row)
+            if(bucket_size_after_insertion <= bucket_to_insert.max_bucket_size):
+                bucket_to_insert.insert(value)
+            else:
+                self.bucket_list = self._balanceBuckets(value)
 
-    def hashFunction(self, hash_key):
-        '''
-        mod2^i hash function
-        '''
-        binary_hash_key = '{0:07b}'.format(hash_key) #todo: δυναμική επιλογή αριθού bits
+        except:
+            print("Error with insert")
 
-        if(binary_hash_key[0] == "0"):
-            bucket_id = 0
-        else:
-            bucket_id = 1
-        return bucket_id
+    def search(self, value):
+        if self.bucket_list == [[]]:
+            print("Hash Table is empty, create first")
+            return
+
+        bucket = self.bucket_list[self.hashFunction(value)]
+
+        for row in bucket.data:
+            if row[0] == value:
+                return row[1]
 
 
+
+db = Database("test_db", True)
+print(db.unlock_table('users'))
+h = HashIndex(db, 64)
+h.createHashtable("users")
+
+# print(h.count_buckets)
+# print(len(h.bucketlist))
+# for bucket in h.bucketlist:
+#     print(bucket, bucket.data)
+
+# print("~~BUCKET LIST~~")
+# print(h.bucketlist)
+# insert a value
+value = 10
+print("find the value ", value, " is the row", h.search(value))
